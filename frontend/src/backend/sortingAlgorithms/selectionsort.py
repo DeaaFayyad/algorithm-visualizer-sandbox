7a98ea1686ae3318f02982a53dc9d8a9fbe30705
Deaa-Fayyad <deaafayyad10@gmail.com>
2025-10-09 15:30:49 +1100

    Small edit on selection sort file

--- frontend/src/backend/sortingAlgorithms/selectionsort.py
+++ frontend/src/backend/sortingAlgorithms/selectionsort.py
@@ -3,22 +3,15 @@ def selection_sort(arr):
     n = len(arr)
     for i in range(n - 1):
       
-        # Assume the current position holds
-        # the minimum element
         min_idx = i
         
-        # Iterate through the unsorted portion
-        # to find the actual minimum
         for j in range(i + 1, n):
             animations.append(("compare", j, min_idx))
             if arr[j] < arr[min_idx]:
               
-                # Update min_idx if a smaller element is found
                 min_idx = j
                 animations.append(("set min", i, min_idx))
-        
-        # Move minimum element to its
-        # correct position
+
         if min_idx != i:
             arr[i], arr[min_idx] = arr[min_idx], arr[i]
             animations.append(("swap", i, min_idx))
